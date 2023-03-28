00c8bb0f40f9deeca52d657d6c02a2c2e56d5ab4
Alvaro Rojas <Rojasalvaro819@gmail.com>
2023-03-28 14:15:18 -0300

    Realizacion de la tarea de la segunda clase

---
+++ segundaClase/SegundaClasejs.js
@@ -0,0 +1,92 @@
+let suma = 5 + 5;
+let resultado = suma;
+
+console.log(resultado)
+
+// Ejercicio 2
+
+var dif1 = 6;
+var dif2 = 4;
+
+console.log(dif1==dif2)
+
+// Ejercicio 3
+
+console.log (dif1!=dif2)
+
+//Ejercicio 4
+
+let aumento = 5;
+aumento++;
+console.log("variable con primer incremento " + aumento)
+aumento++;
+console.log("variable con segundo incremento " +aumento)
+aumento++;
+console.log("variable con tercer incremento " +aumento)
+aumento++;
+console.log("variable con cuarto incremento " +aumento)
+aumento++
+console.log("variable con quinto incremento " +aumento)
+
+console.log("")
+console.log("Ejercicio 5")
+console.log("")
+
+let decremento = 10
+decremento--
+console.log("variable con primer decremento " +decremento)
+decremento--
+console.log("variable con segundo decremento " +decremento)
+decremento-- 
+console.log("variable con tercer decremento " +decremento)
+
+console.log("")
+console.log("ejercicio 6")
+console.log("")
+
+let saludo = "Alvaro";
+console.log ("Bienvenido a rolling code " + saludo)
+
+console.log("")
+console.log("ejercicio 7")
+console.log("")
+
+var dato1 = true;
+var dato2 = false;
+
+console.log(dato1 && dato2)
+
+console.log("")
+console.log("ejercicio 8")
+console.log("")
+
+console.log(dato1||dato2)
+
+console.log("")
+console.log("ejercicio 9")
+console.log("")
+
+var num1 = 10;
+var num2 = 15;
+console.log(num1<num2)
+
+console.log("")
+console.log("ejercicio 10")
+console.log("")
+
+console.log (num1>=num2)
+
+console.log("")
+console.log("ejercicio 11")
+console.log("")
+
+const ingresar = (prompt("Ingrese su nombre"))
+console.log("Hola wachin " + ingresar.toLowerCase().trim() )
+
+console.log("")
+console.log("ejercicio 12")
+console.log("")
+
+let greeting = (prompt("Ingrese su nombre y su saludo"))
+
+console.log("Este este es el saludo de  " + greeting.toUpperCase().trim() )
