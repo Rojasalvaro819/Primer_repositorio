b5dddaf9273328bb5294e7fcfb8a4f0d16fb878e
Alvaro Rojas <Rojasalvaro819@gmail.com>
2023-04-11 15:02:59 -0500

    clonando repositorio en netbook

--- Clase-4/Clase-4_javascript.js
+++ Clase-4/Clase-4_javascript.js
@@ -42,9 +42,9 @@ switch (numberMonth) {
 }
 
 // Ejercicio 2
-console.log("")
-console.log("Ejercicio 2")
-console.log("")
+console.log("");
+console.log("Ejercicio 2");
+console.log("");
 
 let week_Day = parseInt(prompt("Ingrese el numero de dia que quiera saber"));
 switch (week_Day) {
@@ -74,224 +74,214 @@ switch (week_Day) {
 }
 
 // Ejercicio 3
-console.log("")
-console.log("Ejercicio 3")
-console.log("")
+console.log("");
+console.log("Ejercicio 3");
+console.log("");
 
-let vocal = prompt("Ingrese una letra para saber si es vocal o consonante").toLowerCase()
- 
-switch (vocal){
-    case "a":
-        console.log("La letra " + vocal + " es una vocal");
-        break;
-        case "e":
-        console.log("La letra " + vocal + " es una vocal");
-        break;
-        case "i":
-        console.log("La letra " + vocal + " es una vocal");
-        break;
-        case "o":
-        console.log("La letra " + vocal + " es una vocal");
-        break;
-        case "u":
-        console.log("La letra " + vocal + " es una vocal");
-        break;
-        default:
-            console.log("La letra ingresada es una consonante")
+let vocal = prompt(
+  "Ingrese una letra para saber si es vocal o consonante"
+).toLowerCase();
+
+switch (vocal) {
+  case "a":
+    console.log("La letra " + vocal + " es una vocal");
+    break;
+  case "e":
+    console.log("La letra " + vocal + " es una vocal");
+    break;
+  case "i":
+    console.log("La letra " + vocal + " es una vocal");
+    break;
+  case "o":
+    console.log("La letra " + vocal + " es una vocal");
+    break;
+  case "u":
+    console.log("La letra " + vocal + " es una vocal");
+    break;
+  default:
+    console.log("La letra ingresada es una consonante");
 }
 
 // Ejercicio 4
-console.log("")
-console.log("Ejercicio 4")
-console.log("")
+console.log("");
+console.log("Ejercicio 4");
+console.log("");
 
-let day = prompt("Ingrese un numero de dia, del lunes a viernes")
-let days = ["lunes","martes","miercoles","jueves","viernes"]
+let day = prompt("Ingrese un numero de dia, del lunes a viernes");
+let days = ["lunes", "martes", "miercoles", "jueves", "viernes"];
 
-switch (day){
+switch (day) {
   case "1":
-  console.log("El numero " + day + " Es el dia " +  days[0] + " de la semana")
+    console.log("El numero " + day + " Es el dia " + days[0] + " de la semana");
     break;
 
-    case "2":
-      console.log("El numero " + day + " Es el dia " +  days[1] + " de la semana")
-        break;
+  case "2":
+    console.log("El numero " + day + " Es el dia " + days[1] + " de la semana");
+    break;
 
-        case "3":
-          console.log("El numero " + day + " Es el dia " +  days[2] + " de la semana")
-            break;
-            case "4":
-              console.log("El numero " + day + " Es el dia " +  days[3] + " de la semana")
-                break;
-                case "5":
-                  console.log("El numero " + day + " Es el dia " +  days[4] + " de la semana")
-                    break;
+  case "3":
+    console.log("El numero " + day + " Es el dia " + days[2] + " de la semana");
+    break;
+  case "4":
+    console.log("El numero " + day + " Es el dia " + days[3] + " de la semana");
+    break;
+  case "5":
+    console.log("El numero " + day + " Es el dia " + days[4] + " de la semana");
+    break;
 
-    default:
-      console.log("Ingrese un  numero valido")
+  default:
+    console.log("Ingrese un  numero valido");
     break;
 }
 
 // Ejercicio 5
-console.log("")
-console.log("Ejercicio 5")
-console.log("")
+console.log("");
+console.log("Ejercicio 5");
+console.log("");
 
-let letter = prompt("Ingrese una Letra")
+let letter = prompt("Ingrese una Letra");
 
-switch(letter){
+switch (letter) {
   case letter.toLocaleLowerCase():
-    console.log("la letra ingresada es minuscula")
+    console.log("la letra ingresada es minuscula");
     break;
-    case letter.toLocaleUpperCase():
-      console.log("La letra ingresada es mayuscula")
-      break;
+  case letter.toLocaleUpperCase():
+    console.log("La letra ingresada es mayuscula");
+    break;
 }
 
 // Ejercicio 6
-console.log("")
-console.log("Ejercicio 6")
-console.log("")
+console.log("");
+console.log("Ejercicio 6");
+console.log("");
 
-const notas = () =>{
+const notas = () => {
+  let notus = document.getElementById("notus");
+  let af = notus.value;
 
+  console.log(af);
 
-let notus = document.getElementById("notus");
-let af = notus.value;
-
-console.log (af)
-
-switch(af){
-  case "1":
-    console.log("SUSPENSO");
-    break;
+  switch (af) {
+    case "1":
+      console.log("SUSPENSO");
+      break;
     case "2":
-    console.log("SUSPENSO");
-    break;
+      console.log("SUSPENSO");
+      break;
     case "3":
-    console.log("SUSPENSO");
-    break;
+      console.log("SUSPENSO");
+      break;
     case "4":
-    console.log("SUSPENSO");
-    break;
+      console.log("SUSPENSO");
+      break;
     case "5":
-    console.log("Aprobado");
-    break;
+      console.log("Aprobado");
+      break;
     case "6":
-    console.log("Aprobado");
-    break;
+      console.log("Aprobado");
+      break;
     case "7":
-    console.log("Aprobado");
-    break;
+      console.log("Aprobado");
+      break;
     case "8":
-    console.log("Notable");
-    break;
+      console.log("Notable");
+      break;
     case "9":
-    console.log("Notable");
-    break;
+      console.log("Notable");
+      break;
     case "10":
-    console.log("Sobresaliente");
-    break;
+      console.log("Sobresaliente");
+      break;
     default:
-      console.log("Ingrese una nota valida")
+      console.log("Ingrese una nota valida");
+  }
+};
 
-}
-
-}
-
-
-console.log ("Tarea 2 Ejercicios con ciclos o bucles controlados")
-console.log("")
+console.log("Tarea 2 Ejercicios con ciclos o bucles controlados");
+console.log("");
 
 // Ejercicio 1
 
-for (let i = 2; i <=20; i += 2){
-  console.log(i)
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
 }
 
-console.log("")
-console.log("Ejercicio 2")
-console.log("")
+console.log("");
+console.log("Ejercicio 2");
+console.log("");
 
-let frases = ["Hola","Mundo","ALVARO","ROJAS"];
+let frases = ["Hola", "Mundo", "ALVARO", "ROJAS"];
 
-for (let i = 0; i < frases.length;i ++){
+for (let i = 0; i < frases.length; i++) {
   console.log(frases[i].toUpperCase());
 }
 
-console.log("")
-console.log("Ejercicio 3")
-console.log("")
+console.log("");
+console.log("Ejercicio 3");
+console.log("");
 
 // Ejercicio 3
 
-
-
-const impresion = ()=>{
-
-  let numero = document.getElementById("Num")
+const impresion = () => {
+  let numero = document.getElementById("Num");
   let num = numero.value;
-for (let i = 1; i<=num; i ++){
-  console.log(i)
-}
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+};
 
-}
-
-console.log("")
-console.log("Ejercicio 4")
-console.log("")
+console.log("");
+console.log("Ejercicio 4");
+console.log("");
 
 // Ejercicio 4
 
-let uno = parseInt(prompt("Ingrese el primero"))
-let dos= parseInt(prompt("ingrese el segundo"))
-let tres= parseInt(prompt("ingrese el tercero"))
-let cuarto = parseInt(prompt("Ingrese el cuarto"))
+let uno = parseInt(prompt("Ingrese el primero"));
+let dos = parseInt(prompt("ingrese el segundo"));
+let tres = parseInt(prompt("ingrese el tercero"));
+let cuarto = parseInt(prompt("Ingrese el cuarto"));
 
-let  arr = [uno,dos,tres,cuarto]
+let arr = [uno, dos, tres, cuarto];
 
-const minMax=(arr)=>{
-  let min = arr[0]
-  let max = arr[0]
-  for (let i = 0 ; i<arr.length; i ++){
-    if (min>arr[i]){
-      min = arr[i]
+const minMax = (arr) => {
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
     }
-    if (max < arr[i]){
-      max = arr[i]
+    if (max < arr[i]) {
+      max = arr[i];
     }
   }
- console.log("El numero mas grande de los ingresados es " + max)
-}
+  console.log("El numero mas grande de los ingresados es " + max);
+};
 
-minMax(arr)
+minMax(arr);
 
-console.log("")
-console.log("Ejercicio 5")
-console.log("")
+console.log("");
+console.log("Ejercicio 5");
+console.log("");
 
 // Ejercicio 5
 
-let palabra = prompt("Ingrese  una  palabra para saber si es palindromo o no")
-let revertido= palabra.split("").reverse().join("")
+let palabra = prompt("Ingrese  una  palabra para saber si es palindromo o no");
+let revertido = palabra.split("").reverse().join("");
 
-const palindromoONo = (palabra,revertido)=>{
+const palindromoONo = (palabra, revertido) => {
+  if (revertido == palabra) {
+    return console.log(revertido + " Es palindromo");
+  } else {
+    console.log(revertido + " No es palindromo");
+  }
+};
+palindromoONo(palabra, revertido);
 
-if (revertido == palabra){
-  return console.log(revertido + " Es palindromo")
-} else {
-  console.log( revertido + " No es palindromo")
-}
- 
-}
-palindromoONo(palabra,revertido)
-
-console.log("")
-console.log("Ejercicio 6")
-console.log("")
+console.log("");
+console.log("Ejercicio 6");
+console.log("");
 
 // Ejercicio 6
-
 
 const input = prompt("Ingrese una serie de números separados por coma:");
 
@@ -300,7 +290,7 @@ let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
   const num = parseInt(numbers[i]); // convertir el string a número
-  if (num % 2 === 0) { 
+  if (num % 2 === 0) {
     sum += num;
   }
 }
@@ -308,115 +298,74 @@ for (let i = 0; i < numbers.length; i++) {
 // Imprimir la suma de los números pares en la consola
 console.log("La suma de los números pares es: " + sum);
 
-console.log("")
-console.log("Ejercicio 7")
-console.log("")
+console.log("");
+console.log("Ejercicio 7");
+console.log("");
 
 // Ejercicio 7
 
+const inpul = prompt("Ingrese una serie de números separados por coma:");
 
+const number = input.split(",");
 
+let count = 0;
 
-const inpul = prompt(
+for (let i = 0; i < numbers.length; i++) {
+  const num = parseInt(number[i]);
 
-  "Ingrese una serie de números separados por coma:");
-  
-  
-  
-  
-  const number = input.
-  
-  split(",");
-  
-  
-  
-  
-  
-  
-  let count = 0;
-  
-  
-  for (
-   
-  let i = 0; i < numbers.length; i++) {
-    const num = parseInt(number[i]); 
-    
-   
-  if (num < 0) { 
-      count++; 
-      count++;
-  
-      count
-  
-     
- 
-    }
-  }
-  
-  console.log("La cantidad de números negativos es: " + count);
+  if (num < 0) {
+    count++;
+    count++;
 
-  console.log("")
-console.log("Ejercicio 8")
-console.log("")
-
-// Ejercicio 8
-
-
-const num = parseInt(prompt("Ingrese un número:"));
-
-
-for (let i = 1; i <= num; i++) {
-  
- 
-if (i % 2 !== 0) { 
-    
-   
-console.log(i); 
+    count;
   }
 }
 
-console.log("")
-console.log("Ejercicio 9")
-console.log("")
+console.log("La cantidad de números negativos es: " + count);
+
+console.log("");
+console.log("Ejercicio 8");
+console.log("");
+
+// Ejercicio 8
+
+const num = parseInt(prompt("Ingrese un número:"));
+
+for (let i = 1; i <= num; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+console.log("");
+console.log("Ejercicio 9");
+console.log("");
 
 // Ejercicio 9
 
-
 const numeros = prompt("Ingresa una serie de números separados por coma:");
-
 
 const numerosArray = numeros.split(",").map(Number);
 
-
 const numeroMasPequeno = Math.min(...numerosArray);
-
 
 console.log(`El número más pequeño es: ${numeroMasPequeno}`);
 
-
-
-console.log("")
-console.log("Ejercicio 10")
-console.log("")
+console.log("");
+console.log("Ejercicio 10");
+console.log("");
 
 // Ejercicio 10
-
-
-
-
 
 // Generamos un número aleatorio entre 1 y 100
 // const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 
-
 // let contadorIntentos = 0;
 
-
 // function adivinaNumero() {
-  
+
 //   const numeroUsuario = parseInt(prompt("Adivina el número (entre 1 y 100):"));
 
-  
 //   contadorIntentos++;
 
 //   if (numeroUsuario === numeroAleatorio) {
@@ -424,22 +373,19 @@ console.log("")
 //   } else {
 //     const pista = numeroUsuario > numeroAleatorio ? "menor" : "mayor";
 //     console.log(`El número que buscas es ${pista} que ${numeroUsuario}`);
-   
+
 //     adivinaNumero();
 //   }
 // }
 
-
 // adivinaNumero();
 
-
-console.log("")
-console.log("Ejercicio 11")
-console.log("")
+console.log("");
+console.log("Ejercicio 11");
+console.log("");
 
 // Ejercicio 11
 const numeross = prompt("Ingresa una serie de números separados por coma:");
-
 
 const numerosArrayy = numeross.split(",").map(Number);
 
@@ -451,6 +397,5 @@ for (let i = 0; i < numerosArrayy.length; i++) {
     contadorPares++;
   }
 }
-
 
 console.log(`La cantidad de números pares es: ${contadorPares}`);
