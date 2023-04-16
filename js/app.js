f62b6a745a97143eed279e0f53d00313eb19a6b7
Alvaro Rojas <Rojasalvaro819@gmail.com>
2023-04-15 23:03:59 -0300

    Iniciamos repositorio local  de primer proyecto

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let saberEdad = ()=>{
+
+let miEdad = prompt(`Ingrese su edad`)
+let edad = (miEdad>=18)? console.log (`puede pasar usted tiene ${miEdad} `):console.log (`No puede pasar usted tiene ${miEdad}`)
+}
